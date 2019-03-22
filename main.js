fcc5d2d28deac48901973ac435c71361f6fc6e6b
Anna Goodwin <goodwin.musician@gmail.com>
2019-03-22 08:55:05 -0600

    Add name input to submit button functionality

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ var numInput1 = document.querySelector("#guess1");
 var numInput2 = document.querySelector("#guess2");
 var leftBigNumber = document.querySelector(".left-big-number");
 var rightBigNumber = document.querySelector(".right-big-number");
+var challenger1NameDisplay = document.querySelector(".challenger1-name-display");
+var challenger2NameDisplay = document.querySelector(".challenger2-name-display");
+var challenger1NameInput = document.querySelector("#challenger1");
+var challenger2NameInput = document.querySelector("#challenger2")
 var submitButton = document.querySelector("#submit");
+
 
 
 
@@ -25,31 +30,12 @@ update.addEventListener("click", function() {
 submitButton.addEventListener("click", function() {
   leftBigNumber.innerText = numInput1.value;
   rightBigNumber.innerText = numInput2.value;
-
+  challenger1NameDisplay.innerText = challenger1NameInput.value;
+  challenger2NameDisplay.innerText = challenger2NameInput.value;
 });
 
 function getRandomNumSecret(min, max) {
 	min = Math.ceil(userInput).value;
 	max = Math.floor(userInput2).value;
 	return Math.floor(Math.random() * (max - min + 1)) + min;
-
-
-// var challengerOneName =
-// document.querySelector("#challenger1");
-// var challengerOneText =
-// document.querySelector("#guess1");
-// var challengerTwo = 
-// document.querySelector("#challenger2");
-// var challengerTwoGuess =
-// document.querySelector("#guess2");
-// var getUserNameGuessButton = 
-// document.querySelector("#submit");
-
-// getUserNameGuessButton.addEventListener("click",
-//   function () {
-//     var guesses = challengerOneName.value;
-//     console.log(guesses)
-//   }
-
-// var getUserInputButton = 
-// document.querySelector =()
+}
