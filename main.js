a94c052e4085714d1dee2d51022e6878c054bc7e
Taylor Jordan <taylornoel418@icloud.com>
2019-03-25 19:09:19 -0600

    Update error message functions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var minRange = document.querySelector("#min-range-display");
 var maxRange = document.querySelector("#max-range-display");
-var userInput = document.querySelector("#min-range-input");
-var userInput2 = document.querySelector("#max-range-input");
+var userInput = document.querySelector("#minrangeinput");
+var userInput2 = document.querySelector("#maxrangeinput");
 var update = document.querySelector("#update");
 var numInput1 = document.querySelector("#guess1");
 var numInput2 = document.querySelector("#guess2");
@@ -10,7 +10,8 @@ var rightBigNumber = document.querySelector(".right-big-number");
 var challenger1NameDisplay = document.querySelector(".challenger1-name-display");
 var challenger2NameDisplay = document.querySelector(".challenger2-name-display");
 var challenger1NameInput = document.querySelector("#challenger1");
-var challenger2NameInput = document.querySelector("#challenger2")
+var challenger2NameInput = document.querySelector("#challenger2");
+var nameError1 = document.querySelector("#name-error-1");
 var submitButton = document.querySelector("#submit");
 var randomNum = 0;
 var clearButton = document.querySelector("#clear");
@@ -18,44 +19,64 @@ var resetButton = document.querySelector("#reset");
 var challengerOneResults = document.querySelector("#challenger-1-results");
 var challengerTwoResults = document.querySelector("#challenger-2-results");
 
-
+var changeMinRange = 1;
+var changeMaxRange = 100;
 
 
 update.addEventListener("click", function() {
-  var changeMinRange = userInput.value;
+  changeMinRange = parseInt(userInput.value);
   minRange.innerHTML = changeMinRange;
   console.log(userInput.value);
-  var changeMaxRange = userInput2.value;
+  changeMaxRange = parseInt(userInput2.value);
   maxRange.innerHTML = changeMaxRange;
   console.log(userInput2.value);
   randomNum = getRandomNumSecret(changeMinRange, changeMaxRange);
   console.log(randomNum);
+  //update range values for range rule in validation function
+  $( "#guess1" ).rules( "add", {
+  required: true,
+  range: [changeMinRange, changeMaxRange],
+  messages: {
+    required: "Required input",
+    // range: jQuery.validator.format("Enter a number between {0} and {1}")
+    }
+  });
+  $( "#guess2" ).rules( "add", {
+  required: true,
+  range: [changeMinRange, changeMaxRange],
+  messages: {
+    required: "Required input",
+   }
+  });
 });
 
 
-submitButton.addEventListener("click", function() {
-  leftBigNumber.innerText = numInput1.value;
-  rightBigNumber.innerText = numInput2.value;
-  challenger1NameDisplay.innerText = challenger1NameInput.value;
-  challenger2NameDisplay.innerText = challenger2NameInput.value;
-  clearButton.classList.remove("disabled");
-  resetButton.classList.remove("disabled");
-  if(parseInt(numInput1.value) < randomNum) {
-  	challengerOneResults.innerHTML = "That's too low!";
-  } else if(parseInt(numInput1.value) === randomNum) {
-  	challengerOneResults.innerHTML = "BOOM!";
-  } else {
-  	challengerOneResults.innerHTML = "That's too high!";
-  }
-  if(parseInt(numInput2.value) > randomNum) {
-    challengerTwoResults.innerHTML = "That's too high!";
-  } else if(parseInt(numInput2.value) < randomNum) {
-    challengerTwoResults.innerHTML = "That's too low!";
-  } else {
-    challengerTwoResults.innerHTML = "BOOM!";
-  }
+// submitButton.addEventListener("click", function() {
+//   // leftBigNumber.innerText = numInput1.value;
+//   rightBigNumber.innerText = numInput2.value;
+//   challenger1NameDisplay.innerText = challenger1NameInput.value;
+//   challenger2NameDisplay.innerText = challenger2NameInput.value;
+//   clearButton.classList.remove("disabled");
+//   resetButton.classList.remove("disabled");
+//   if(parseInt(numInput1.value) < randomNum) {
+//   	challengerOneResults.innerHTML = "That's too low!";
+//   } else if(parseInt(numInput1.value) === randomNum) {
+//   	challengerOneResults.innerHTML = "BOOM!";
+//   } else {
+//   	challengerOneResults.innerHTML = "That's too high!";
+//   }
+//  if(parseInt(numInput2.value) < randomNum) {
+//     challengerTwoResults.innerHTML = "That's too low!";
+//   } else if(parseInt(numInput2.value) === randomNum) {
+//     challengerTwoResults.innerHTML = "BOOM!";
+//   } else {
+//     challengerTwoResults.innerHTML = "That's too high!";
+//   }
+//   if(challenger1NameInput.value === undefined) {
+//     nameError1.classList.remove("hidden")
+//   }
 
-});
+// });
 
 resetButton.addEventListener("click", function() {
   clearButton.classList.add("disabled");
@@ -91,4 +112,6 @@ function getRandomNumSecret(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+};	
+
+
