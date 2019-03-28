e0f86222c360e7489c61997a1b9bdba5e503c0ff
Taylor Jordan <taylornoel418@icloud.com>
2019-03-28 10:45:51 -0600

    Delete js folder

---
+++ pseudocode.js
@@ -0,0 +1,10 @@
+//if guess is <= minRange, then return error message//
+//if guess is >= maxRange, then return error message//
+//query userInput and and userInput2 inputs//
+//query numInput1, numInput2//
+//query submitButton//
+
+// make a form for all user input
+//call that form in jQuery by class or Id
+// use form.validate
+// set rules: guess1 and guess2 
